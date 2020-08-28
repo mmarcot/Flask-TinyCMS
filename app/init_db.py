01e87d69b8a8ab1d0322f2f3e2e33f7e4318b941
mma <mmarcot@gmail.com>
2020-08-28 09:54:37 +0200

    NEw project architecture

---
+++ app/init_db.py
@@ -0,0 +1,51 @@
+from app import db, User, Post, Tag, Page
+
+db.drop_all()
+db.create_all()
+
+admin = User(username='admin', email='amin', password='admin')
+db.session.add(admin)
+db.session.commit()
+
+python_tag = Tag(name='Python')
+web_tag = Tag(name='Web')
+db.session.add(python_tag)
+db.session.add(web_tag)
+db.session.commit()
+
+post1 = Post(
+    title="Le Python c'est sympa", 
+    slug='le-python-c-est-sympa',
+    abstract_image="http://placekitten.com/400/300",
+    abstract="Franchement, oui, on peut dire que le python c'est pas mal du tout. Lol. Aller @+ pour de nouvelles aventures !",
+    content="Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du  (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsumproviennent de la section 1.10.32.L'extrait standard de Lorem Ipsum utilisé depuis le XVIè siècle est reproduit ci-dessous pour les curieux. Les sections 1.10.32 et 1.10.33 du Cicéron sont aussi reproduites dans leur version originale, accompagnée de la traduction anglaise de H. Rackham (1914).",
+    user_id=admin.id,
+    published=True,
+)
+post2 = Post(
+    title="Le python me rend ZINZIN", 
+    slug='le-python-me-rend-zinzin',
+    abstract_image="http://placekitten.com/400/300",
+    abstract="Franchement, oui, on peut dire que le python me rend ZINZIN. Lol. Aller @+ pour de nouvelles aventures !",
+    content="Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du  (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsumproviennent de la section 1.10.32.L'extrait standard de Lorem Ipsum utilisé depuis le XVIè siècle est reproduit ci-dessous pour les curieux. Les sections 1.10.32 et 1.10.33 du Cicéron sont aussi reproduites dans leur version originale, accompagnée de la traduction anglaise de H. Rackham (1914).",
+    user_id=admin.id,
+    published=True,
+)
+post1.tags.append(python_tag)
+post2.tags.append(python_tag)
+post2.tags.append(web_tag)
+db.session.add(post1)
+db.session.add(post2)
+db.session.commit()
+
+page = Page(
+    title="La passion du poulet",
+    slug="la-passion-du-poulet",
+    content="<p>Le poulet cest trop bien</p><p>Ca pond des oeufs!</p>",
+    published=True,
+    user_id=admin.id,
+)
+db.session.add(page)
+db.session.commit()
+
+print('Database initialized')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import LoginManager, login_user, login_required, current_user, UserMixin, logout_user
 
 
 
@@ -11,12 +12,19 @@ from datetime import datetime
 #######################################################################################################
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'makes-me-oulououuua766d'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 
@@ -30,7 +38,7 @@ db = SQLAlchemy(app)
 
 tags = db.Table('tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-    db.Column('article_id', db.Integer, db.ForeignKey('article.id'), primary_key=True)
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
 )
 
 
@@ -44,8 +52,8 @@ class Post(db.Model):
     creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     published = db.Column(db.Boolean, nullable=False, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('articles', lazy=True))
-    tags = db.relationship('Tag', secondary=tags, lazy='subquery', backref=db.backref('articles', lazy=True))
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery', backref=db.backref('posts', lazy=True))
 
 
 class Page(db.Model):
@@ -59,7 +67,7 @@ class Page(db.Model):
     user = db.relationship('User', backref=db.backref('pages', lazy=True))
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
@@ -83,7 +91,31 @@ class Tag(db.Model):
 
 @app.route('/')
 def index():
-    return 'zinzin'
+    return render_template('index.html')
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    user = User.query.filter_by(email=request.form['mail_or_username']).first()
+    if not user:
+        user = User.query.filter_by(username=request.form['mail_or_username']).first()
+    if user and user.password == request.form['password']:
+        login_user(user, remember=True)
+        return redirect(url_for('admin'))
+
+    flash('Bad login')
+    return render_template('login.html')
+
+
+@app.route('/admin')
+@login_required
+def admin():
+    return render_template('admin.html')
+
+
 
 if __name__ == "__main__":
     app.run()
