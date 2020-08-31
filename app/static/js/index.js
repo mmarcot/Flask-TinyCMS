fe1f1ab385a0f9ec4679be3d4727628b938f8801
Mallory MARCOT <mmarcot@gmail.com>
2020-08-31 12:05:49 +0200

    slug auto filling with the current title for posts and pages

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-
+    let slug = document.querySelector('form input[name="slug"]');
+    let title = document.querySelector('form input[name="title"]');
+    
+    if(slug && title) {
+        function convertToSlug(Text) {
+            return Text
+                .toLowerCase()
+                .replace(/ /g,'-')
+                .replace(/[^\w-]+/g,'')
+                ;
+        }
+        slug.setAttribute('disabled', false);
+        title.addEventListener('input', function(event) {
+            slug.value = convertToSlug(title.value);
+        });
+    }
 });
