f5829222b46b6821a003bc974fe0a8a60e435116
Mallory MARCOT <mmarcot@gmail.com>
2020-09-04 12:45:13 +0200

    Lagytext translation for WTForms forms

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import InputRequired, Email, Length, Optional
 from flask_login import LoginManager, login_user, login_required, current_user, UserMixin, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_babel import _, Babel
+from flask_babel import _, Babel, lazy_gettext
 
 from datetime import datetime
 import json
@@ -66,55 +66,55 @@ def flash_form_errors(form):
 #######################################################################################################
 
 class PostCreateForm(FlaskForm):
-    title = StringField(_('Title'), validators=[Length(1,199,_("The title should contain between 1 and 200 characters"))])
-    slug = StringField(_('Slug'), validators=[Length(1,199,_("The slug should contain between 1 and 200 characters"))])
-    tags = StringField(_('Tags'), render_kw={"data-role":"tagsinput"})
-    published = BooleanField(_('Published'), default=True)
-    abstract_image = StringField(_('Abstract image'), validators=[Optional(),Length(1,199,_("The link to the image should contain between 1 and 200 characters"))])
-    abstract = TextAreaField(_('Abstract'), validators=[Optional()])
-    content = TextAreaField(_('Content'))
+    title = StringField(lazy_gettext('Title'), validators=[Length(1,199,lazy_gettext("The title should contain between 1 and 200 characters"))])
+    slug = StringField(lazy_gettext('Slug'), validators=[Length(1,199,lazy_gettext("The slug should contain between 1 and 200 characters"))])
+    tags = StringField(lazy_gettext('Tags'), render_kw={"data-role":"tagsinput"})
+    published = BooleanField(lazy_gettext('Published'), default=True)
+    abstract_image = StringField(lazy_gettext('Abstract image'), validators=[Optional(),Length(1,199,lazy_gettext("The link to the image should contain between 1 and 200 characters"))])
+    abstract = TextAreaField(lazy_gettext('Abstract'), validators=[Optional()])
+    content = TextAreaField(lazy_gettext('Content'))
     form_type = HiddenField()
-    submit = SubmitField(_('Create'))
+    submit = SubmitField(lazy_gettext('Create'))
 
 class PostEditForm(PostCreateForm):
-    submit = SubmitField(_('Save'))
+    submit = SubmitField(lazy_gettext('Save'))
 
 
 class PageCreateForm(FlaskForm):
-    title = StringField(_('Title'), validators=[Length(1,199,_("The title should contain between 1 and 200 characters"))])
-    slug = StringField(_('Slug'), validators=[Length(1,199,_("The slug should contain between 1 and 200 characters"))])
-    nav_label = StringField(_('Navigation label'), validators=[Length(1,49,_("The navigation label should contain between 1 and 50 characters"))])
-    published = BooleanField(_('Published'), default=True)
-    content = TextAreaField(_('Content'))
+    title = StringField(lazy_gettext('Title'), validators=[Length(1,199,lazy_gettext("The title should contain between 1 and 200 characters"))])
+    slug = StringField(lazy_gettext('Slug'), validators=[Length(1,199,lazy_gettext("The slug should contain between 1 and 200 characters"))])
+    nav_label = StringField(lazy_gettext('Navigation label'), validators=[Length(1,49,lazy_gettext("The navigation label should contain between 1 and 50 characters"))])
+    published = BooleanField(lazy_gettext('Published'), default=True)
+    content = TextAreaField(lazy_gettext('Content'))
     form_type = HiddenField()
-    submit = SubmitField(_('Create'))
+    submit = SubmitField(lazy_gettext('Create'))
 
 class PageEditForm(PageCreateForm):
-    submit = SubmitField(_('Save'))
+    submit = SubmitField(lazy_gettext('Save'))
 
 
 class UserCreateForm(FlaskForm):
-    username = StringField(_('Username'), validators=[InputRequired(_("Username cannot be empty")), Length(5,49,_("The username should contain between 5 and 50 characters"))])
-    email = StringField(_('Email'), validators=[InputRequired(_("Merci de saisir une adresse e-mail.")), Email(_("The given e-mail is not valid"))])
-    password = PasswordField(_('Password'), validators=[Length(5,49,_("The password should contain between 5 and 50 characters"))])
-    submit = SubmitField(_('Create'))
+    username = StringField(lazy_gettext('Username'), validators=[InputRequired(lazy_gettext("Username cannot be empty")), Length(5,49,lazy_gettext("The username should contain between 5 and 50 characters"))])
+    email = StringField(lazy_gettext('Email'), validators=[InputRequired(lazy_gettext("Email cannot be empty")), Email(lazy_gettext("The given e-mail is not valid"))])
+    password = PasswordField(lazy_gettext('Password'), validators=[Length(5,49,lazy_gettext("The password should contain between 5 and 50 characters"))])
+    submit = SubmitField(lazy_gettext('Create'))
 
 class UserEditForm(UserCreateForm):
-    password = PasswordField(_('Password'), validators=[Optional(), Length(5,49,_("The password should contain between 5 and 50 characters"))])
-    submit = SubmitField(_('Save'))
+    password = PasswordField(lazy_gettext('Password'), validators=[Optional(), Length(5,49,lazy_gettext("The password should contain between 5 and 50 characters"))])
+    submit = SubmitField(lazy_gettext('Save'))
 
 
 class TagCreateForm(FlaskForm):
-    name = StringField(_('Name'), validators=[Length(2,49, "The tag name should contain between 2 and 50 characters")])
-    submit = SubmitField(_('Create'))
+    name = StringField(lazy_gettext('Name'), validators=[Length(2,49, lazy_gettext("The tag name should contain between 2 and 50 characters"))])
+    submit = SubmitField(lazy_gettext('Create'))
 
 class TagEditForm(TagCreateForm):
-    submit = SubmitField(_('Save'))
+    submit = SubmitField(lazy_gettext('Save'))
 
 
 class AdminConfigurationForm(FlaskForm):
-    language = SelectField(_('Language'), choices=LANGUAGES)
-    submit = SubmitField(_('Save'))
+    language = SelectField(lazy_gettext('Language'), choices=LANGUAGES)
+    submit = SubmitField(lazy_gettext('Save'))
 
 
 
